f7130d45aac704ddf561d2bc30ddbdc0dfb9a47b
Emily Jirles <emily.jirles@gmail.com>
2018-04-24 21:34:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-                      [0,1,2], #top row 
-                      [3,4,5], #mid row
-                      [6,7,8], #bot row
-                      [0,3,6], #left col 
-                      [1,4,7], #mid col 
-                      [2,5,8], #right col 
-                      [0,4,8], #l>r diag 
-                      [2,4,6]  #r>l diag 
-                    ]
+  [0,1,2], #top row 
+  [3,4,5], #mid row
+  [6,7,8], #bot row
+  [0,3,6], #left col 
+  [1,4,7], #mid col 
+  [2,5,8], #right col 
+  [0,4,8], #l>r diag 
+  [2,4,6]  #r>l diag 
+]
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
